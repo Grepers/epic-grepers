bc0bb1ecfa69003d1b05f1712d16124686c79a5c
Fernando Farias <yiowns@gmail.com>
2018-03-11 18:34:21 -0300

    Improving jackson.

--- src/main/java/com/grepers/epicgrepers/EpicGrepersApplication.java
+++ src/main/java/com/grepers/epicgrepers/EpicGrepersApplication.java
@@ -9,18 +9,33 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Spring application implementation.
+ */
 @SpringBootApplication
 @EnableScheduling
 public class EpicGrepersApplication {
 
+    /**
+     * Jackson mapper definitions.
+     *
+     * @param builder Mapper builder.
+     * @return Object mapper.
+     */
     @Bean
     @Primary
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        //TODO jackson mapper : serialize times in ISO mode.
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
     }
 
+    /**
+     * Main entry point.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         SpringApplication.run(EpicGrepersApplication.class, args);
     }
--- src/main/java/com/grepers/epicgrepers/config/WSConfig.java
+++ src/main/java/com/grepers/epicgrepers/config/WSConfig.java
@@ -6,17 +6,23 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+/**
+ * Websocket Spring configuration.
+ */
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSocket
 public class WSConfig implements WebSocketConfigurer {
 
-    private final WSHandler wsHandler;
+    private final WSClientHandler wsClientHandler;
 
+    /**
+     * Register handlers per websocket.
+     *
+     * @param registry Registry from bean.
+     */
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry
-                .addHandler(wsHandler, "/client")
+        registry.addHandler(wsClientHandler, "/client")
                 .setAllowedOrigins("*");
     }
-
 }
