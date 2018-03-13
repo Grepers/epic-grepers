0ae05b716f7197b57783d04d38eacc7086ddf6d8
Fernando Farias <yiowns@gmail.com>
2018-03-13 20:50:01 -0300

    Adding support for maps.

--- src/main/java/com/grepers/epicgrepers/world/World.java
+++ src/main/java/com/grepers/epicgrepers/world/World.java
@@ -1,15 +1,20 @@
 package com.grepers.epicgrepers.world;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.grepers.epicgrepers.collisions.CollisionDetection;
 import javafx.geometry.Point2D;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.math3.util.Pair;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -19,6 +24,7 @@ import java.util.stream.Collectors;
 /**
  * World simulation,
  */
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class World {
@@ -30,9 +36,27 @@ public class World {
     @Value("${map}")
     private String map;
 
+    /**
+     * Setup this world.
+     */
     @PostConstruct
     public final void setup() {
-        //TODO load map
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+        try {
+            JsonNode rootNode = objectMapper.readTree(getClass().getResource(map));
+            rootNode.fields().forEachRemaining(entry -> {
+                //TODO cargar actores desde el mapa en yml se podra hacer con reflection ?
+                if (entry.getKey().equals("Wall")) {
+                    JsonNode value = entry.getValue();
+                    actors.add(new Wall(
+                            new Point2D(value.findValue("x").asDouble(), value.findValue("y").asDouble()),
+                            value.findValue("width").asDouble(),
+                            value.findValue("height").asDouble()));
+                }
+            });
+        } catch (IOException e) {
+            log.error("Cannot load map.", e);
+        }
     }
 
     /**
